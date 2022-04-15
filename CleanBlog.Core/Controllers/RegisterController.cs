c4fa265423116e2d0cff585b89e785cf8f9e2368
lengoquyen0411 <lengoquyen0411@gmail.com>
2022-04-15 23:29:05 +0700

    update composing & controllers

---
+++ CleanBlog.Core/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using CleanBlog.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
+using Umbraco.Core.Services.Implement;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace CleanBlog.Core.Controllers
+{
+    /// <summary>
+    /// Handle member registration
+    /// </summary>
+    public class RegisterController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/";
+
+        public RegisterController()
+        {
+        }
+
+        public ActionResult RenderRegister()
+        {
+            var vm = new RegisterViewModel();
+            return PartialView(PARTIAL_VIEW_FOLDER + "Register.cshtml", vm);
+        }
+
+        
+
+        
+    }
+}
